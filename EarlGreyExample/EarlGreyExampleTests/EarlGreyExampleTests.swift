3fedd50b334444ccabf7222beb50a21687809031
Bastian Veliz Vega <bastian.veliz@schibsted.com>
2018-07-16 20:24:39 -0400

    Initial setup

---
+++ EarlGreyExample/EarlGreyExampleTests/EarlGreyExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  EarlGreyExampleTests.swift
+//  EarlGreyExampleTests
+//
+//  Created by Bastián Véliz Vega on 16-07-18.
+//  Copyright © 2018 Bastián Véliz Vega. All rights reserved.
+//
+
+import XCTest
+@testable import EarlGreyExample
+
+class EarlGreyExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
